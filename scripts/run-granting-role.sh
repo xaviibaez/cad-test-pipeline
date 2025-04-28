19fbde31fd87409075be16ad16f7bbf3d197d6db
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-28 12:29:04 +0200

    update granting role script

--- scripts/run-granting-role.sh
+++ scripts/run-granting-role.sh
@@ -4,20 +4,9 @@ set -o pipefail
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 
 echo "Running granting role script"
-echo "$@"
-echo "1-"
-echo $@
-echo "2-"
 echo $GO_TRIGGER_USER
-echo "3-"
 echo ${GO_TRIGGER_USER}
-echo "4-"
-echo '$GO_TRIGGER_USER'
-echo "5-"
-echo '{$GO_TRIGGER_USER}'
-echo "6-"
-echo `{$GO_TRIGGER_USER}`
-echo "7-"
-echo `$@`
-echo "8-"
 echo "${!@}"
+
+cd ~/godata/config
+cat cruise-config.xml
