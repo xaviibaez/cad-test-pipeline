73f374ae4cc8795fb2d2d4933d5216a3f96fb63c
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-29 08:58:17 +0200

    update granting role script 6

--- scripts/run-granting-role.sh
+++ scripts/run-granting-role.sh
@@ -24,6 +24,9 @@ echo "Im here 5: "
 cd config
 ls
 
+echo "Finding cruise-config.xml: "
+find / -name "cruise-config.xml" 2>/dev/null
+
 cat config/cruise-config.xml
 
 sed '/<role name="release-manager">/,/<\/role>/ {
