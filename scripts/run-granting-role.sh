5ed9bb8b15870ed141f1a1832179cd01716ab3d0
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-29 17:30:27 +0200

    update granting role script 15

--- scripts/run-granting-role.sh
+++ scripts/run-granting-role.sh
@@ -10,7 +10,7 @@ echo ${GO_TRIGGER_USER}
 echo "Doing sed: "
 sed '/<role name="release-manager">/,/<\/role>/ {
     /<users>/a\            <user>'$GO_TRIGGER_USER'</user>
-}' /shared_data/cruise-config.xml > output.xml
+}' /shared_data/cruise-config.xml > /shared_data/output.xml
 
 echo "Im here 4: "
 cat /shared_data/output.xml
