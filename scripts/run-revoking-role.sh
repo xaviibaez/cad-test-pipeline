b47608eec24a03a0cb035f2bd09da15696976382
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-29 08:22:32 +0200

    update revoking role script

--- scripts/run-revoking-role.sh
+++ scripts/run-revoking-role.sh
@@ -3,5 +3,17 @@ set -o errexit
 set -o pipefail
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 
-echo "Running revoking role script"
-echo "$@"
+echo "Running revoking role script for: "
+echo $GO_TRIGGER_USER
+echo ${GO_TRIGGER_USER}
+echo "${!@}"
+
+cd ..
+cat godata/config/cruise-config.xml
+
+sed '/<role name="release-manager">/,/<\/role>/ {
+    /<user>'"${!@}"'<\/user>/d
+}' cruise-config.xml > output.xml
+
+mv output.xml cruise-config.xml
+rm -f output.xml
