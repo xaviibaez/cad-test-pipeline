ad8c71566a424b0fe48cde9c524102115944db45
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-30 10:08:51 +0200

    update grating role script 20

--- scripts/run-granting-role.sh
+++ scripts/run-granting-role.sh
@@ -7,7 +7,11 @@ echo "Running granting role script for: "
 echo $GO_TRIGGER_USER
 echo ${GO_TRIGGER_USER}
 
-echo "Doing sed: "
-sed -i '/<role name="release-manager">/,/<\/role>/ {
-    /<users>/a\            <user>'$GO_TRIGGER_USER'</user>
-}' /shared_data/cruise-config.xml
+if grep -q "<role name=\"release-manager\">.*<user>${GO_TRIGGER_USER}</user>" /shared_data/cruise-config.xml; then
+    echo "User ${GO_TRIGGER_USER} already exists in the release-manager role. Skipping addition."
+else
+    echo "Adding user ${GO_TRIGGER_USER} to the release-manager role."
+    sed -i '/<role name="release-manager">/,/<\/role>/ {
+        /<users>/a\            <user>'$GO_TRIGGER_USER'</user>
+    }' /shared_data/cruise-config.xml
+fi
