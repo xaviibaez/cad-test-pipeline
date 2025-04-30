1a73ccfb08e1814ad53048640540af88f7c74072
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-04-30 16:14:09 +0200

    update grating role script 22

--- scripts/run-granting-role.sh
+++ scripts/run-granting-role.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -o errexit
 set -o pipefail
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 
@@ -7,13 +6,16 @@ echo "Running granting role script for: "
 echo $GO_TRIGGER_USER
 echo ${GO_TRIGGER_USER}
 
-xmllint --xpath '//role[@name="release-manager"]/users/user[text()="'$GO_TRIGGER_USER'"]' shared_data/cruise-config.xml >/dev/null 2>&1
-if [ $? -eq 0 ]; then
+fgrep -A5 '<role name="release-manager">' /shared_data/cruise-config.xml | fgrep -q '<user>'$GO_TRIGGER_USER'</user>'
+if [ $? = 0 ]; then
     echo "User ${GO_TRIGGER_USER} already exists in the release-manager role."
-    exit 0
+    exit 1
 fi
 
-echo "Adding user ${GO_TRIGGER_USER} to the release-manager role."
-sed -i '/<role name="release-manager">/,/<\/role>/ {
-    /<users>/a\            <user>'$GO_TRIGGER_USER'</user>
-}' /shared_data/cruise-config.xml
+sed '/<role name="release-manager">/,/<\/role>/ {
+    /<users>/a\
+            <user>'$GO_TRIGGER_USER'</user>
+}' /shared_data/cruise-config.xml > /shared_data/output.xml
+
+mv /shared_data/output.xml /shared_data/cruise-config.xml
+rm -f /shared_data/output.xml
